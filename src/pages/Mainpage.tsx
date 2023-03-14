21ccb0af3c61e774f171ddc0a92f73eef0f73e4f
yundonghui <qeadzc9503@naver.com>
2023-03-14 23:04:18 +0900

    [#1] Feat: 차트 중앙 정렬 스타일 적용

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,16 +1,46 @@
 import useDataList from '../hooks/useDataList';
-import { Suspense } from 'react';
+import styled from 'styled-components';
 
 export default function Mainpage() {
   const { dataList, isLoading } = useDataList();
 
+  if (isLoading) {
+    return <div>로딩중</div>;
+  }
   return (
-    <Suspense fallback={<div>로딩중</div>}>
-      <ul>
-        {dataList.map(data => {
-          return <div>{data.timestamp}</div>;
-        })}
-      </ul>
-    </Suspense>
+    <WrapperDiv>
+      <ContentDiv>
+        <ChartDiv>
+          <div>value_bar</div>
+          <ul>
+            {dataList.map(({ timestamp, data }) => {
+              return <li key={timestamp}>{data.value_bar}</li>;
+            })}
+          </ul>
+        </ChartDiv>
+      </ContentDiv>
+    </WrapperDiv>
   );
 }
+
+const ChartDiv = styled.div`
+  border: solid black 1px;
+  width: 500px;
+  height: 500px;
+`;
+
+const WrapperDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #301e4e;
+`;
+
+const ContentDiv = styled.div`
+  font-family: system-ui, serif;
+  font-size: 2rem;
+  padding: 3rem;
+  border-radius: 1rem;
+  background: #ff6e6c;
+`;
