3309a8d2242a38643dc65c5b2bf6aec755ea7342
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-16 03:48:47 +0900

    [#1] refactor: 툴팁 항목을 유동적으로 생성할 수 있도록 수정

--- src/utils/createToolTip.ts
+++ src/utils/createToolTip.ts
@@ -13,12 +13,10 @@ const createToolTip = ({
   id: string[];
 }) => {
   const { series, dataPointIndex, w } = options;
-  const barColor = w.globals.colors[0];
-  const areaColor = w.globals.colors[1];
-  const barValue = series[0][dataPointIndex].toLocaleString();
-  const areaValue = series[1][dataPointIndex].toLocaleString();
   const idValue = id[dataPointIndex];
   const time = w.config.xaxis.categories[dataPointIndex];
+  const colors = w.globals.colors;
+  const configSeries = w.config.series;
 
   return `
   <div style="border-radius: 5px; box-shadow: 2px 2px 6px -4px #999; cursor: default; font-size: 14px; border: 1px solid #e3e3e3; background: #fff; overflow: hidden; white-space: nowrap;">
@@ -27,16 +25,24 @@ const createToolTip = ({
       <span style="font-size: 12px; opacity: 0.8;">${time}</span>
     </div>
     <ul>
-      <li style="display: flex; align-items: center; justify-content: left; padding: 6px 10px 5px;">
-        <span style="width: 12px; height: 12px; position: relative; top: 0px; margin-right: 10px; border-radius: 50%; background-color: ${barColor};" aria-hidden="true"></span>
-        <span>Bar: </span>
-        <strong style="display: inline-block; font-weight: 600; margin-left: 5px;">${barValue}</strong>
-      </li>
-      <li style="display: flex; align-items: center; justify-content: left; padding: 6px 10px 5px;">
-        <span style="width: 12px; height: 12px; position: relative; top: 0px; margin-right: 10px; border-radius: 50%; background-color: ${areaColor};" aria-hidden="true"></span>
-        <span>Area: </span>
-        <strong style="display: inline-block; font-weight: 600; margin-left: 5px;">${areaValue}</strong>
-      </li>
+      ${series.reduce((acc: string, crr: string, idx: number) => {
+        if (!!crr.length) {
+          return (acc += `
+            <li style="display: flex; align-items: center; justify-content: left; padding: 6px 10px 5px;">
+              <span style="width: 12px; height: 12px; position: relative; top: 0px; margin-right: 10px; border-radius: 50%; background-color: ${
+                colors[idx]
+              };" aria-hidden="true"></span>
+              <span>${configSeries[idx].name}: </span>
+              <strong style="display: inline-block; font-weight: 600; margin-left: 5px;">${crr[
+                dataPointIndex
+              ].toLocaleString()}</strong>
+            </li>
+          `);
+        }
+        return acc;
+      }, '')}
+
+     
     </ul>
   </div>
   `;
