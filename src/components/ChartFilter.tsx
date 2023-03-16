a7be2e14c94e7abbff08c507474c73f823792b58
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-17 08:22:55 +0900

    [#3] feat: 필터 버튼 영역 컴포넌트 구현

---
+++ src/components/ChartFilter.tsx
@@ -0,0 +1,40 @@
+import useMockList from '../hooks/useMockList';
+import useFilterList from './../hooks/useFilterList';
+import Button from './common/Button';
+import { MouseEvent } from 'react';
+import styled from 'styled-components';
+
+const ChartFilter = () => {
+  const { idList } = useMockList();
+  const { currentFilter, setFilter } = useFilterList('id');
+
+  const idSet = ['전지역', ...new Set(idList)];
+
+  const onClickHandler = (e: MouseEvent<HTMLElement>) => {
+    const name = (e.target as HTMLElement).getAttribute('name') as string;
+    setFilter(name);
+  };
+
+  return (
+    <ChartFilterWrapper>
+      {idSet.map((id, idx) => (
+        <Button
+          key={idx}
+          value={id}
+          onClickHandler={onClickHandler}
+          isActive={id === currentFilter || !idx}
+        />
+      ))}
+    </ChartFilterWrapper>
+  );
+};
+
+const ChartFilterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  margin: 40px 0 8px;
+`;
+
+export default ChartFilter;
