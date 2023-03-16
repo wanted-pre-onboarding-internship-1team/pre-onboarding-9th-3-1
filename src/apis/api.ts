61575e4fc0a0c611c393a88c03f0ef3fc12755ca
Byeong Min Cho <merrybmc@gmail.com>
2023-03-16 12:11:48 +0900

    [#1 #2] fix : api

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import React from 'react';
 
 interface Data {
   [key: string]: {
@@ -9,9 +10,9 @@ interface Data {
 }
 
 const url =
-  'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/19736c70-c41c-4a4a-b45f-b866eebab626/mock_data_example-flexsys.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230314%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230314T164943Z&X-Amz-Expires=86400&X-Amz-Signature=c61f0fe02a714b982c83b5241f231719eba966960060b2a83d946f351077128e&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data_example-flexsys.json%22&x-id=GetObject';
+  'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/19736c70-c41c-4a4a-b45f-b866eebab626/mock_data_example-flexsys.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230316%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230316T031055Z&X-Amz-Expires=86400&X-Amz-Signature=143540a3cecbca5c00c386b93cf9719ae87baf42694eaa9c20bfe464cdf367f2&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data_example-flexsys.json%22&x-id=GetObject';
 
-export const fetchData = async () => {
+export const getData = async () => {
   const response = await axios.get<{ response: Data }>(url);
   const data = response.data.response;
   return data; // 데이터 반환
--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,6 +1,6 @@
+import { getData } from '../apis/api';
 import CombinedChart from '../common/CombinedChart';
 import { Layout } from '../style/Layout.styled';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 interface Data {
@@ -18,20 +18,18 @@ interface ParsedData {
   value_bar: number;
 }
 
-const Mainpage = () => {
+export default function Mainpage() {
   const [mockData, setMockData] = useState<Data>({});
   const [parsedData, setParsedData] = useState<ParsedData[]>([]);
 
   useEffect(() => {
-    const getData = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(
-          'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/19736c70-c41c-4a4a-b45f-b866eebab626/mock_data_example-flexsys.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230315%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230315T184813Z&X-Amz-Expires=86400&X-Amz-Signature=ea321f977fdf015c5dadc6b1de8aa2a8943eaf6e5e2657cb7c34e5dcce0c540f&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data_example-flexsys.json%22&x-id=GetObject'
-        );
-        setMockData(response.data.response);
+        const data = await getData();
+        setMockData(data);
       } catch (error) {}
     };
-    getData();
+    fetchData();
   }, []);
 
   useEffect(() => {
@@ -53,6 +51,4 @@ const Mainpage = () => {
       </Layout>
     </div>
   );
-};
-
-export default Mainpage;
+}
