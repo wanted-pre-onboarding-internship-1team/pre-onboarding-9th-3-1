b7f8e67f8f68dfb81d1f7efbf7f0a73cd374d349
gong25 <60168937+gong25@users.noreply.github.com>
2023-03-16 19:43:43 +0900

    [#2]feat: 차트데이터 표시, 차트 툴팁 표시

--- src/hooks/useChartdata.jsx
+++ src/hooks/useChartdata.jsx
@@ -1,349 +1,55 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { getChartData } from '../apis/api';
 
+function createTooltip(value) {
+  // console.log(data);
+  return `<div style="padding:5px 5px 5px 5px;"><div><strong>${value.id}</strong></div><div><p>bar: ${value.value_bar}</p><p>area: ${value.value_area}</p></div> </div>`;
+}
+
 const useChartdata = () => {
+  const [loading, setLoading] = useState(false);
   const [chartData, setChartData] = useState([]);
-  // const [chartDataTable, setChartDataTable] = useState([]);
+  // const chartTable = useRef();
 
   useEffect(() => {
-    getChartData().then(({ data }) => setChartData(data));
+    setLoading(true);
+    getChartData()
+      .then(({ data }) => {
+        const newDataArray = Object.entries(data).map(([key, value]) => {
+          return [
+            key,
+            value.value_bar,
+            createTooltip(value),
+            value.value_area,
+            createTooltip(value),
+            value.id,
+          ];
+        });
+
+        const dataTable = [
+          [
+            { label: 'Time', type: 'string' },
+            { label: 'value_bar', type: 'number' },
+            { role: 'tooltip', type: 'string', p: { html: true } },
+            { label: 'value_area', type: 'number' },
+            { role: 'tooltip', type: 'string', p: { html: true } },
+            { label: 'id', type: 'string' },
+          ],
+          ...newDataArray,
+        ];
+        setChartData(dataTable);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
-  const formatData = new Promise(resolve => {
-    const keyArray = ['time', ...Object.keys(chartData)];
-    const valueArray = [
-      ...Object.values(chartData).map(item => Object.values(item)),
-    ];
-    resolve({ keyArray: keyArray, valueArray: valueArray });
-  });
+  // console.log(dataTable);
+  // setChartTable([...dataTable]);
+  // chartTable.current = dataTable;
+  // setLoading(false);
 
-  formatData.then(data => {
-    // console.log('프라미스');
-    // console.log(data);
-    const chartDataTable = [[...data.keyArray], [...data.valueArray]];
-    console.log(chartDataTable);
-    // setChartDataTable([[data.keyArray], [data.valueArray]]);
-  });
-
-  return chartData;
+  return [loading, chartData];
 };
 
 export default useChartdata;
-
-function drawComboChart() {
-  let jsonData = {
-    cols: [
-      {
-        label: 'Game',
-        type: 'string',
-      },
-      {
-        label: 'Win',
-        type: 'number',
-      },
-      {
-        role: 'tooltip',
-        type: 'string',
-      },
-      {
-        label: 'Defeat',
-        type: 'number',
-      },
-      {
-        role: 'tooltip',
-        type: 'string',
-      },
-      {
-        label: 'Draw',
-        type: 'number',
-      },
-      {
-        role: 'tooltip',
-        type: 'string',
-      },
-      {
-        label: 'Opponent score',
-        type: 'number',
-      },
-      {
-        role: 'tooltip',
-        type: 'string',
-      },
-    ],
-    rows: [
-      {
-        c: [
-          {
-            v: 646,
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 646 0:10 lost',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 646 0:10 lost',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 646 0:10 lost',
-          },
-          {
-            v: 10,
-          },
-          {
-            v: 'Game 646 0:10 lost',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 647,
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 647 0:13 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 647 0:13 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 647 0:13 won',
-          },
-          {
-            v: 13,
-          },
-          {
-            v: 'Game 647 0:13 won',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 648,
-          },
-          {
-            v: 22,
-          },
-          {
-            v: 'Game 648 22:23 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 648 22:23 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 648 22:23 won',
-          },
-          {
-            v: 23,
-          },
-          {
-            v: 'Game 648 22:23 won',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 651,
-          },
-          {
-            v: 13,
-          },
-          {
-            v: 'Game 651 13:31 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 651 13:31 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 651 13:31 won',
-          },
-          {
-            v: 31,
-          },
-          {
-            v: 'Game 651 13:31 won',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 652,
-          },
-          {
-            v: 85,
-          },
-          {
-            v: 'Game 652 85:75 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 652 85:75 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 652 85:75 won',
-          },
-          {
-            v: 75,
-          },
-          {
-            v: 'Game 652 85:75 won',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 653,
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 653 0:10 lost',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 653 0:10 lost',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 653 0:10 lost',
-          },
-          {
-            v: 10,
-          },
-          {
-            v: 'Game 653 0:10 lost',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 654,
-          },
-          {
-            v: 18,
-          },
-          {
-            v: 'Game 654 18:12 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 654 18:12 won',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 654 18:12 won',
-          },
-          {
-            v: 12,
-          },
-          {
-            v: 'Game 654 18:12 won',
-          },
-        ],
-      },
-      {
-        c: [
-          {
-            v: 655,
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 655 10:22 lost',
-          },
-          {
-            v: 10,
-          },
-          {
-            v: 'Game 655 10:22 lost',
-          },
-          {
-            v: 0,
-          },
-          {
-            v: 'Game 655 10:22 lost',
-          },
-          {
-            v: 22,
-          },
-          {
-            v: 'Game 655 10:22 lost',
-          },
-        ],
-      },
-    ],
-  };
-  let dt = new google.visualization.DataTable(jsonData);
-  //dt.addColumn({type: 'string', role: 'tooltip'});
-  let options = {
-    width: 480,
-    height: 360,
-    legend: {
-      position: 'top',
-      alignment: 'center',
-      maxLines: 2,
-    },
-    seriesType: 'steppedArea',
-    series: {
-      3: {
-        type: 'line',
-        lineWidth: 1,
-        color: '000000',
-        lineDashStyle: [4, 2],
-      },
-    },
-  };
-
-  let chart = new google.visualization.ComboChart(
-    document.getElementById('chart_div')
-  );
-  chart.draw(dt, options);
-}
--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,24 +1,56 @@
 import { useEffect, useState } from 'react';
-import { Chart } from 'react-google-charts';
+import { ReactGoogleChartEvent, Chart } from 'react-google-charts';
 import { getChartData } from '../apis/api';
 import useChartdata from '../hooks/useChartdata';
 
 export default function Mainpage() {
-  const chartData = useChartdata();
+  const [loading, dataTable] = useChartdata();
+
+  const options = {
+    title: '플렉시스 chart',
+    seriesType: 'bars',
+    series: { 1: { type: 'area' } },
+    legend: 'none',
+    tooltip: { isHtml: true },
+  };
+
+  const chartEvents: ReactGoogleChartEvent[] = [
+    {
+      eventName: 'select',
+      callback: ({ chartWrapper }) => {
+        const chart = chartWrapper.getChart();
+        const selection = chart.getSelection();
+        if (selection.length === 1) {
+          const [selectedItem] = selection;
+          const dataTable = chartWrapper.getDataTable();
+          const { row, column } = selectedItem;
+
+          console.log(chartWrapper);
+
+          console.log('You selected:', {
+            row,
+            column,
+            value: dataTable?.getValue(row, column),
+          });
+        }
+      },
+    },
+  ];
+
+  const renderChart = loading
+    ? 'Loading.....'
+    : dataTable && (
+        <Chart
+          chartType='ComboChart'
+          data={dataTable}
+          width='100%'
+          height='400px'
+          options={options}
+          chartEvents={chartEvents}
+        />
+      );
 
   return (
-    <div style={{ width: '1200px', margin: '50px auto' }}>
-      <Chart
-        chartType='ScatterChart'
-        data={[
-          ['Age', 'Weight'],
-          [4, 5.5],
-          [8, 12],
-        ]}
-        width='100%'
-        height='400px'
-        legendToggle
-      />
-    </div>
+    <div style={{ width: '1200px', margin: '50px auto' }}>{renderChart}</div>
   );
 }
